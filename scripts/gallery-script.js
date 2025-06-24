09eb0149f64f9a51accb37f20b5d847d680d219d
ShenruiXue666 <146133221+ShenruiXue666@users.noreply.github.com>
2025-06-24 16:24:57 +0800

    Update gallery-script.js

--- scripts/gallery-script.js
+++ scripts/gallery-script.js
@@ -38,7 +38,7 @@ async function loadMessages() {
 
     // 留言内容（文字气泡）
     const text = document.createElement("p");
-    text.textContent = message || "（无文字）";
+    text.textContent = message || "（）";
     card.appendChild(text);
 
     // 时间戳
